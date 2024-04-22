083f6ecf78de4b3086eb5dd955ac67ebeaff8b29
Mihir1119 <156939360+Mihir1119@users.noreply.github.com>
2024-04-22 22:33:45 +0530

    Add files via upload

---
+++ Web Tech Project/English1.js
@@ -0,0 +1,73 @@
+function Result()
+{
+    let total = 0;
+
+    var o1 = document.getElementById("flexRadioDefault1_1");
+    if (o1.checked) {
+        total= total + 1;
+    }
+    var o2 = document.getElementById("flexRadioDefault2_2");
+    if (o2.checked) {
+        total = total + 1;
+    }
+    var o3 = document.getElementById("flexRadioDefault3_4");
+    if(o3.checked){
+        total = total + 1;
+    }
+    var o4 = document.getElementById("flexRadioDefault4_1");
+    if(o4.checked){
+        total = total + 1;
+    }
+    var o5 = document.getElementById("flexRadioDefault5_1");
+    if(o5.checked){
+        total = total + 1;
+    }
+    var o6 = document.getElementById("flexRadioDefault6_3");
+    if(o6.checked){
+        total = total + 1;
+    }
+    var o7 = document.getElementById("flexRadioDefault7_1");
+    if(o7.checked){
+        total = total + 1;
+    }
+    var o8 = document.getElementById("flexRadioDefault8_2");
+    if(o8.checked){
+        total = total + 1;
+    }
+    var o9 = document.getElementById("flexRadioDefault9_2");
+    if(o9.checked){
+        total = total + 1;
+    }
+    var o10 = document.getElementById("flexRadioDefault10_4");
+    if(o10.checked){
+        total = total + 1;
+    }
+    var o11 = document.getElementById("flexRadioDefault11_1");
+    if(o11.checked){
+        total = total + 1;
+    }
+    var o12 = document.getElementById("flexRadioDefault12_3");
+    if(o12.checked){
+        total = total + 1;
+    }
+    var o13 = document.getElementById("flexRadioDefault13_2");
+    if(o13.checked){
+        total = total + 1;
+    }
+    var o14 = document.getElementById("flexRadioDefault14_1");
+    if(o14.checked){
+        total = total + 1;
+    }
+    var o15 = document.getElementById("flexRadioDefault15_3");
+    if(o15.checked){
+        total = total + 1;
+    }
+    
+    let total_1 = (total / 15) * 100;
+
+    let total_2 = total_1.toFixed(2);
+
+    localStorage.setItem("total", total_2);
+
+    location.href = "Result.html"
+}
